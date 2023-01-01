c5a993e231c83fe70a28a9d1c8e63f23408f654f
cxnturies <khollushkin22@gmail.com>
2023-01-01 19:11:41 +0300

    Добавьте файлы проекта.

---
+++ pp_kholushkin/Controllers/WeatherForecastController.cs
@@ -0,0 +1,30 @@
+﻿using Contracts;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pp_kholushkin.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class WeatherForecastController : ControllerBase
+	{
+		private ILoggerManager _logger;
+		public WeatherForecastController(ILoggerManager logger)
+		{
+			_logger = logger;
+		}
+		[HttpGet]
+		public IEnumerable<string> Get()
+		{
+			_logger.LogInfo("Вот информационное сообщение от нашего контроллера значений.");
+			_logger.LogDebug("Вот отладочное сообщение от нашего контроллера значений.");
+			_logger.LogWarn("Вот сообщение предупреждения от нашего контроллера значений.");
+			_logger.LogError("Вот сообщение об ошибке от нашего контроллера значений.");
+			return new string[] { "value1", "value2" };
+		}
+	}
+}
